f1f670eeb92b83978d2bc7d4166c8ac18d0e812c
taohong1999 <2834647668@qq.com>
2018-09-12 01:43:14 +0800

    the first homework of CSharp class

---
+++ HomeWork1/Program1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "";
+            double n1 = 0;
+            double n2 = 0;
+            double theAdd;
+            Console.Write("Please input an int: ");
+            s = Console.ReadLine();
+            n1 = double.Parse(s);
+            Console.Write("Please input another int: ");
+            s = Console.ReadLine();
+            n2 = double.Parse(s);
+            theAdd = n1 * n2;
+            Console.WriteLine("The product of two ints is : " + theAdd);
+        }
+    }
+}
---
+++ HomeWork1/Program2/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Program2
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string s1 = textBox1.Text;
+            string s2 = textBox2.Text;
+            double n1 = double.Parse(s1);
+            double n2 = double.Parse(s2);
+            double theAdd = n1 * n2;
+            label3.Text = "两个数的乘积是：" + theAdd;
+        }
+    }
+}
