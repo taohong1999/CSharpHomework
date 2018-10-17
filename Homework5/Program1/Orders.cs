163b566fb65ef4a91fd8ff1c4e9abe3dba1c9354
taohong1999 <2834647668@qq.com>
2018-10-17 21:51:02 +0800

    The fifth CSharphomework

---
+++ Homework5/Program1/Orders.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program2
+{
+    //订单的基本信息
+    class Orders
+    {
+        internal string orderNumber;
+        internal string theCustomer;
+        internal double money;
+        public List<OrderDetails> mOrderDetails = new List<OrderDetails>();
+        public Orders(string ordernumber, string thecustomer)
+        {
+            this.orderNumber = ordernumber;
+            this.theCustomer = thecustomer;
+        }
+
+        public void AddGood(string name, double price, int number)
+        {
+            OrderDetails orderdetails = new OrderDetails(name, price, number);
+            mOrderDetails.Add(orderdetails);
+            this.money += price;
+        }
+
+        public void DelGood(string name)
+        {
+            OrderDetails Detail = null;
+            foreach (OrderDetails details in mOrderDetails)
+            {
+                if (details.goodName == name)
+                {
+                    Detail = details;
+                }
+            }
+            if (Detail == null) { Console.WriteLine("未找到该商品"); }
+            else
+            {
+                this.money -= Detail.goodPrice;
+                mOrderDetails.Remove(Detail);
+            }
+        }
+    }
+}
---
+++ Homework5/Program2/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Program2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string s = textBox1.Text;
+            Pre1 = double.Parse(s); 
+            s = textBox2.Text;
+            Pre2 = double.Parse(s);
+            s = textBox3.Text;
+            th1 = double.Parse(s) * Math.PI / 180;
+            s = textBox4.Text;
+            th2 = double.Parse(s) * Math.PI / 180;
+            s = textBox5.Text;
+            k = double.Parse(s);
+
+            if (graphics == null) graphics = this.CreateGraphics();
+            drawCayleyTree(10, 200, 310, 100, -Math.PI / 2);
+        }
+
+        private Graphics graphics;
+        double th1 = 30 * Math.PI / 180;
+        double th2 = 20 * Math.PI / 180;
+        double Pre1 = 0.6;
+        double Pre2 = 0.7;
+        double k = 0.5;
+        void drawCayleyTree(int n, double x0, double y0, double length, double th)
+        {
+            if (n == 0) return;
+
+            double x1 = x0 + length * Math.Cos(th);
+            double y1 = y0 + length * Math.Sin(th);
+
+            double x2 = x0 + k * length * Math.Cos(th);
+            double y2 = y0 + k * length * Math.Sin(th);
+
+
+            drawLine(x0, y0, x1, y1);
+
+            drawCayleyTree(n - 1, x1, y1, Pre1 * length, th + th1);
+            drawCayleyTree(n - 1, x2, y2, Pre2 * length, th - th2);
+        }
+
+        void drawLine(double x0, double y0, double x1, double y1)
+        {
+            graphics.DrawLine(Pens.Red, (int)x0, (int)y0, (int)x1, (int)y1);
+        }
+    }
+}
