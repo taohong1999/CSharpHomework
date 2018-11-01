89210c21fe5978c2a370ad20fe96c16d2b4a33b1
taohong1999 <2834647668@qq.com>
2018-11-01 14:25:01 +0800

    The seventh CSharphomework

---
+++ Homework7/Program2/Form1.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using Program1;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Program2
+{
+    public partial class Form1 : Form
+    {
+        internal OrderService service = new OrderService();
+        private OrderService mservice = new OrderService();  //用于储存符合条件的order
+        public Form1()
+        {
+            InitializeComponent();
+
+            AddService();
+            bindingSerives.DataSource = service;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void AddService()
+        {
+            if (File.Exists(@"orders.xml"))
+            {
+                service = OrderService.Import(@"orders.xml");
+            }
+            else
+            {
+                Orders order1 = new Orders("123456","Tom");
+                order1.AddGood("电脑", 5000, 1);
+                Orders order2 = new Orders("321654","Jack");
+                order2.AddGood("华为honor10", 3000, 1);
+                Orders order3 = new Orders("987654","Fremont");
+                order3.AddGood("iWatch", 2000, 1);
+                service.AddOrders2(order1);
+                service.AddOrders2(order2);
+                service.AddOrders2(order3);
+            }
+        }
+
+        //查询订单
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            //bindingSerives.DataSource = service;
+            mservice.mOrders.Clear();
+            Orders order;
+            string str1 = textBox1.Text;
+            string str2 = comboBox1.Text;
+            bool isSeach = false;
+            if (str2 == null || str1 == null)
+            {
+                MessageBox.Show("信息未输入");
+                return;
+            }
+            else if (str2 == "订单号")
+            {
+                for (int i = 0; i < service.mOrders.Count; i++)
+                {
+                    order = service.mOrders[i];
+                    if (order.orderNumber == str1)
+                    {
+                        mservice.AddOrders2(order);
+                        isSeach = true;
+                    }
+                }
+                if (isSeach == false) MessageBox.Show("未找到订单");
+            }
+
+            else if (str2 == "姓名")
+            {
+                for (int i = 0; i < service.mOrders.Count; i++)
+                {
+                    order = service.mOrders[i];
+                    if (order.theCustomer == str1)
+                    {
+                        mservice.AddOrders2(order);
+                        isSeach = true;
+                    }
+                }
+                if (isSeach == false) MessageBox.Show("未找到订单");
+            }
+            else MessageBox.Show("未找到订单");
+            bindingSerives.DataSource = mservice;
+        }
+
+        //查看订单明细
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Orders orders = new Orders();
+            string str1 = label2.Text;
+            string str2 = label3.Text;
+            for (int i = 0; i < service.mOrders.Count; i++)
+            {
+                if (service.mOrders[i].orderNumber == str1 && service.mOrders[i].theCustomer == str2)
+                {
+                    orders = service.mOrders[i];
+                    break;
+                }
+            }
+            bindingOrders.DataSource = orders;
+        }
+
+        //添加订单
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Form2 form2 = new Form2(this);
+            form2.Show();
+        }
+
+        //删除订单
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Orders orders = new Orders();
+            string str1 = label2.Text;
+            string str2 = label3.Text;
+            for (int i = 0; i < service.mOrders.Count; i++)
+            {
+                if (service.mOrders[i].orderNumber == str1 && service.mOrders[i].theCustomer == str2)
+                {
+                    orders = service.mOrders[i];
+                    break;
+                }
+            }
+            service.DeleteOrder2(orders);
+            BindingSource bs = new BindingSource();
+            bs.DataSource = service;
+            bindingSerives.DataSource = bs;
+        }
+
+        //修改订单
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Orders orders = new Orders();
+            string str1 = label2.Text;
+            string str2 = label3.Text;
+            for (int i = 0; i < service.mOrders.Count; i++)
+            {
+                if (service.mOrders[i].orderNumber == str1 && service.mOrders[i].theCustomer == str2)
+                {
+                    orders = service.mOrders[i];
+                    break;
+                }
+            }
+            Form2 form2 = new Form2(this, orders);
+            form2.Show();
+        }
+
+
+
+        public void ReOrders()
+        {
+            BindingSource bs = new BindingSource();
+            bs.DataSource = service;
+            bindingSerives.DataSource = bs;
+        }
+
+
+        //多余无用但无法删除
+        private void dataGridView2_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+        }
+
+        private void bindingSerives_CurrentChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void bindingOrders_CurrentChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+        }
+
+      
+    }
+}
---
+++ Homework7/Program2/Form2.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using Program1;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Program2
+{
+    public partial class Form2 : Form
+    {
+
+
+        Orders orders = new Orders();
+        Form1 form1;
+        public Form2(Form1 form)
+        {
+            InitializeComponent();
+
+            this.label1.Text = "添加订单";
+            this.button1.Text = "添加";
+            this.label2.Text = "客户姓名 ";
+            this.label3.Text = "订单号";
+            bindingSource1.DataSource = orders;
+            this.form1 = form;
+        }
+
+        public Form2(Form1 form, Orders order)
+        {
+            InitializeComponent();
+
+            this.label1.Text = "修改订单";
+            this.button1.Text = "确认修改";
+            this.label2.Text = "客户姓名 ";
+            this.label3.Text = "订单号";
+
+            this.orders = order;
+            this.textBox1.Text = order.theCustomer;
+            this.textBox2.Text = order.orderNumber;
+            bindingSource1.DataSource = orders;
+            this.form1 = form;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string str1 = textBox1.Text;
+            string str2 = textBox2.Text;
+            if (this.label1.Text == "添加订单")
+            {
+               
+                if (str1 == "" || str2 == "")
+                {
+                    MessageBox.Show("信息未输入完整");
+                }
+                else
+                {
+                    orders.theCustomer = str1;
+                    orders.orderNumber = str2;
+                    orders.ReMoney();
+                    form1.service.AddOrders2(orders);
+                }
+            }
+
+            else if(this.label1.Text == "修改订单")
+            {
+                orders.theCustomer = str1;
+                orders.orderNumber = str2;
+                orders.ReMoney();
+                //bindingSource1.DataSource = orders;               
+            }
+            form1.ReOrders();
+            this.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OrderDetails details = null;
+            string str = label5.Text;
+            for (int i = 0; i < orders.mOrderDetails.Count; i++)
+            {
+                if (orders.mOrderDetails[i].goodName == str)
+                {
+                    details = orders.mOrderDetails[i];
+                    break;
+                }
+            }
+            orders.mOrderDetails.Remove(details);
+            BindingSource bs = new BindingSource();
+            bs.DataSource = orders;
+            bindingSource1.DataSource = bs;
+        }
+
+        private void panel3_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
